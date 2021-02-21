ce81ebe4660507bc29e1aec83d151506dba84341
595937 <bowen_kenneth@bah.com>
2021-02-21 12:06:13 -0600

    my course updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
+import { TodoEditorComponent } from './todo-editor/todo-editor.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component: LoginComponent},
+  {path: 'login' , component: LoginComponent},
+  {path: 'welcome' , component: WelcomeComponent, canActivate:[RouteGuardService]},
+  {path: 'welcome/:name' , component: WelcomeComponent, canActivate:[RouteGuardService]},
+  {path: 'todos', component: ListTodosComponent,canActivate:[RouteGuardService]},
+  {path: 'logout', component: LogoutComponent,canActivate:[RouteGuardService]},  
+  {path: 'editTodo/:id', component: TodoEditorComponent, canActivate:[RouteGuardService]},
+  {path: '**', component: ErrorComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { Ken1Component } from './ken1/ken1.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoutComponent } from './logout/logout.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TodoEditorComponent } from './todo-editor/todo-editor.component';
+import { HttpInterceptorBasicAuthService } from './service/http/http-interceptor-basic-auth.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    Ken1Component,
+    LoginComponent,
+    ErrorComponent,
+    ListTodosComponent,
+    MenuComponent,
+    FooterComponent,
+    LogoutComponent,
+    TodoEditorComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorBasicAuthService, multi:true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
